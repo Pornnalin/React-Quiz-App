da5b7baa0572f4a5625e1337a490dd1fd78d1847
MaiSang <sarusasiri@gmail.com>
2024-11-20 10:04:06 +0700

    create homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,17 @@
-import { useEffect, useState } from "react";
-import ShowQuestion from "./components/ShowQuestion";
-import ResultScore from "./components/ResultScore";
+import HomePage from "./pages/home";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 function App() {
-  const [index, setIndex] = useState(0);
-  const [score, setScore] = useState(0);
-  const [answer, setAnswer] = useState<string[]>([]);
-  const [end, setEnd] = useState<boolean>(false);
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <HomePage />,
+    },
+  ]);
 
-  const addAnswer = (answerText: string) => {
-    setAnswer([...answer, answerText]);
-  };
-  useEffect(() => {
-    console.log(answer);
-  }, [answer]);
-
-  const handleNewQuestion = () => {
-    const nextIndex = index + 1;
-    setIndex(nextIndex);
-  };
-  const isEnd = (isFinish: boolean) => {
-    setEnd(isFinish);
-  };
-
-  const updateScore = (newScore: number) => {
-    setScore(newScore);
-  };
   return (
-    <div className="flex flex-col justify-center items-center h-screen">
-      {!end && (
-        <div className="bg-white shadow-lg rounded-xl p-20">
-          <ShowQuestion
-            index={index}
-            addAnswer={addAnswer}
-            handleNewQuestion={handleNewQuestion}
-            isFinish={isEnd}
-          />
-        </div>
-      )}
-      {end && (
-        <ResultScore answerList={answer} score={score} setScore={updateScore} />
-      )}
+    <div className="min-h-[100vh]">
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ src/pages/home.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import ResultScore from "../components/ResultScore";
+import ShowQuestion from "../components/ShowQuestion";
+
+function HomePage() {
+  const [index, setIndex] = useState(0);
+  const [score, setScore] = useState(0);
+  const [answer, setAnswer] = useState<string[]>([]);
+  const [end, setEnd] = useState<boolean>(false);
+
+  const addAnswer = (answerText: string) => {
+    setAnswer([...answer, answerText]);
+  };
+  useEffect(() => {
+    console.log(answer);
+  }, [answer]);
+
+  const handleNewQuestion = () => {
+    const nextIndex = index + 1;
+    setIndex(nextIndex);
+  };
+  const isEnd = (isFinish: boolean) => {
+    setEnd(isFinish);
+  };
+
+  const updateScore = (newScore: number) => {
+    setScore(newScore);
+  };
+  return (
+    <div className="flex flex-col justify-center items-center h-screen">
+      {!end && (
+        <div className="bg-white shadow-lg rounded-xl p-20">
+          <ShowQuestion
+            index={index}
+            addAnswer={addAnswer}
+            handleNewQuestion={handleNewQuestion}
+            isFinish={isEnd}
+          />
+        </div>
+      )}
+      {end && (
+        <ResultScore answerList={answer} score={score} setScore={updateScore} />
+      )}
+    </div>
+  );
+}
+export default HomePage;
